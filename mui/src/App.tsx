7a826e799e99d2d83c020c8feceac89daca928ff
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-04 19:36:27 +0500

    Used the developed components to check if ti work properly

--- mui/src/App.tsx
+++ mui/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import MuiButtons from './components/MuiButtons'
 import MuiTypography from './components/MuiTypography'
+import MuiTextfield from './components/MuiTextfield'
+import MuiSelect from './components/MuiSelect'
 
 function App() {
 
   return (
     <div className="App">
-      <MuiTypography />
-      <MuiButtons />
+      {/* <MuiTypography /> */}
+      {/* <MuiButtons /> */}
+      {/* <MuiTextfield /> */}
+      <MuiSelect />
     </div>
   )
 }
