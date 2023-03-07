1d1e03cf63ff9b107bb4346689362a45451c93e1
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-07 16:24:33 +0500

    How to create an autocomplete input field using MUI

---
+++ mui/src/components/MuiAutocomplete.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Stack, Autocomplete, TextField } from '@mui/material'
+
+type Skill = {
+    id: number,
+    label: string
+}
+
+const array = ['HTML', 'CSS', 'Javascript', 'PHP', 'Typescript']
+// Creating data from above array
+const skillOption = array.map((arr, index) => ({
+    id: index + 1,
+    label: arr
+}))
+const MuiAutocomplete = () => {
+        {/* Example of tracking it as a string */}
+    const [value, setValue] = useState<string | null>(null)
+
+      {/* Example How to track the values in an object */}
+    const [skill, setSkill] = useState<Skill | null>(null)
+    console.log({ skill })
+  return (
+    <Stack spacing={2} width='300px'>
+        {/* Example of tracking it as a string */}
+      <Autocomplete options={array} renderInput={(params) => <TextField label='Skills' {...params}/>}
+       value={value} onChange={(event: any, newValue: string | null) => {setValue(newValue)}} 
+      />
+
+      {/* Example How to track the values in an object */}
+      <Autocomplete options={skillOption} renderInput={(params) => <TextField label='Skills' {...params}/>}
+       value={skill} onChange={(event: any, newValue: Skill | null) => {setSkill(newValue)}} //free Solo 'Prop canbe used to allow free input'
+      />
+    </Stack>
+  )
+}
+
+export default MuiAutocomplete
