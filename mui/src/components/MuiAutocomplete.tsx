db5252feab70687e8023aabe17a7d7a86d389235
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-07 16:35:26 +0500

    Updated few props

--- mui/src/components/MuiAutocomplete.tsx
+++ mui/src/components/MuiAutocomplete.tsx
@@ -18,16 +18,17 @@ const MuiAutocomplete = () => {
 
       {/* Example How to track the values in an object */}
     const [skill, setSkill] = useState<Skill | null>(null)
+    console.log({value})
     console.log({ skill })
   return (
     <Stack spacing={2} width='300px'>
         {/* Example of tracking it as a string */}
-      <Autocomplete options={array} renderInput={(params) => <TextField label='Skills' {...params}/>}
+      <Autocomplete options={array} renderInput={(params) => <TextField variant='standard' label='Skills' {...params}/>}
        value={value} onChange={(event: any, newValue: string | null) => {setValue(newValue)}} 
       />
 
       {/* Example How to track the values in an object */}
-      <Autocomplete options={skillOption} renderInput={(params) => <TextField label='Skills' {...params}/>}
+      <Autocomplete options={skillOption} renderInput={(params) => <TextField variant='standard' color='warning' label='Skills' {...params}/>}
        value={skill} onChange={(event: any, newValue: Skill | null) => {setSkill(newValue)}} //free Solo 'Prop canbe used to allow free input'
       />
     </Stack>
